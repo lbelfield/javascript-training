0dd73e7dd84dacc86e6b7b40fc9ef336828a24b7
lbelfield <l.belfield@yahoo.com>
2020-10-28 16:17:07 +0000

    functional vs procedural (Perf vs Readability)

---
+++ 4 Functions/0/functionalVsProcedural.js
@@ -0,0 +1,14 @@
+// Functional programming style VS Procedural programming style
+// JavaScript is a functional programming language. 
+// It gives you API functions (like filter(), map(), reduce() etc) and the ability to pipe these functions.
+
+// API Functions are good at:
+// Readability :thumbs-up: vs Performance :thumbs-down:
+
+// If iterating over hundreds-of-thousands of items, then performance is hit significantly.
+// Therefore, it is important to know your data set.
+
+// Basically a toss up between readability and performance.
+
+// Below is a for loop vs foreach loop vs reduce:
+// https://www.measurethat.net/Benchmarks/Show/2472/0/arrayreduce-vs-for-loop-vs-arrayforeach#latest_results_block
