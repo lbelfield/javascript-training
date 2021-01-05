1cbc53e2a1ef2e569e0262d4621a1921b24f6b99
Jeremy Le François <jeremy.lefrancois@thetrainline.com>
2021-01-05 13:32:58 +0000

    miniDom

---
+++ 10 React/2 miniDom.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+/*
+  - var state = 0
+  - click evt handler on + and -
+    - increment / decrement state
+    - document.getElementById('p_element').innerHTML = `Count: ${state}`
+*/
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const onIncrement = () => {
+    setCount(count + 1);
+  };
+  const onDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Basic counter example</h1>
+      
+      {count !== 0 ? (<p>Count: {count}</p>) : null}
+
+      <button onClick={onDecrement}>-</button> &nbsp;
+      <button onClick={onIncrement}>+</button>
+    </div>
+  );
+}
+
+// If jsx is split into two; state/code (line 1-20) and HTML (after return line 21-33)
+
+// Each component has a mini DOM in the return (line 22-29)
+// See in DevTools > Elements tab: elements get added/removed/replaced in real time after a user interaction
+// when react does a state mutation (change in the DOM) it will do a diff and add/remove/replace the new change
+// So you can think of each component as having a mini DOM that adds to the bigger DOM
+// Think of perf for this...
+
+// Every time there is a mutation, it will ALWAYS re-run the component's code (can check with console log)
+// Whether there is a new state change that affects this component or not, we will always run the code in that component
